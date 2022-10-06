a0b5e9a51602d5adfdcd3350d2b5842f7be8e41f
Dennis Acosta <email@dennisacosta.me>
2022-10-05 20:12:29 -0400

    Add portfolio items

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,31 +4,35 @@ import Heading from '../../helpers/Heading';
 import PortfolioItems from './PortfolioItems';
 
 import pattern from '../../assets/images/pattern-sm-steez.webp';
+import nielsen from '../../assets/images/nielsen-logo.webp';
+import kaplan from '../../assets/images/kaplan-logo.webp';
+import equinox from '../../assets/images/equinox-logo.webp';
+import nwf from '../../assets/images/nwf-logo.webp';
 
 const CONTENT = [
     {
         title: 'Nielsen',
-        location: 'NYC',
-        content:
-            'A global leader in audience insights, data and analytics, Nielsen shapes the future of media with accurate measurement of what people listen to and watch.',
+        img: nielsen,
+        width: '500',
+        height: '76',
     },
     {
         title: 'Kaplan',
-        location: 'LA',
-        content:
-            'Kaplan is the leading global provider of educational programs with comprehensive learning strategies and courses to help anyone succeed.',
-    },
-    {
-        title: 'Halo',
-        location: 'NYC',
-        content:
-            'Replace your dated dog fence, GPS collar, activity tracker, & training collar with one smart system. Shop wireless dog fence solution from Halo.',
+        img: kaplan,
+        width: '500',
+        height: '258',
     },
     {
         title: 'Equinox',
-        location: 'NYC',
-        content:
-            'Equinox is a temple of well-being, featuring world-class personal trainers, group fitness classes, and spas. Voted Best Gym in America by Fitness Magazine.',
+        img: equinox,
+        width: '500',
+        height: '95',
+    },
+    {
+        title: 'National Wildlife Federation',
+        img: nwf,
+        width: '500',
+        height: '198',
     },
 ];
 
@@ -46,6 +50,7 @@ const Portfolio = () => {
 };
 
 const Wrapper = styled.section`
+    justify-content: space-between;
     min-height: 100vh;
     margin: auto;
     padding: var(--space-lg-padding);
--- src/components/Portfolio/PortfolioItems.js
+++ src/components/Portfolio/PortfolioItems.js
@@ -1,59 +1,66 @@
 import styled from 'styled-components';
 
-import selectedMedia from '../../assets/images/selected-clients-img.webp';
-
 import Heading from '../../helpers/Heading';
-import BodyText from '../../helpers/BodyText';
 
 const PortfolioItems = ({ items }) => {
     return (
         <Wrapper>
-            <StyledFigure>
-                <img
-                    src={selectedMedia}
-                    alt='Selected Clients'
-                    width='387'
-                    height='660'
-                />
-            </StyledFigure>
-            <StyledDiv>
-                <Heading level={2}>Nielsen</Heading>
-                <StyledHeading level={4}>NYC</StyledHeading>
-                <BodyText>
-                    A global leader in audience insights, data and analytics,
-                    Nielsen shapes the future of media with accurate measurement
-                    of what people listen to and watch.
-                </BodyText>
-            </StyledDiv>
+            {items.map(({ title, img, width, height }, index) => (
+                <StyledDiv key={index}>
+                    <StyledFigure>
+                        <img
+                            src={img}
+                            alt='Client Logo'
+                            width={width}
+                            height={height}
+                        />
+                    </StyledFigure>
+                    <StyledHeading level={3}>{title}</StyledHeading>
+                </StyledDiv>
+            ))}
         </Wrapper>
     );
 };
 
 const Wrapper = styled.div`
-    align-items: center;
-    display: flex;
-    justify-content: space-between;
-    margin: auto;
-    width: var(--width-md);
+    border-top: 1px solid #fff;
+    margin-left: auto;
+    max-width: 70%;
 `;
 
 const StyledFigure = styled.figure`
-    flex: 0 0 40%;
+    left: -25%;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 20%;
 
     img {
+        display: block;
         height: auto;
-        width: 100%;
+        opacity: 0;
+        transition: visibility 0s 0.3s, opacity 0.3s linear;
+        visibility: hidden;
     }
 `;
 
 const StyledDiv = styled.div`
-    flex: 0 0 50%;
+    align-items: center;
+    border-bottom: 1px solid #fff;
+    justify-content: space-between;
+    padding: 32px 0;
+    position: relative;
+
+    &:hover img {
+        opacity: 1;
+        transition: opacity 0.3s linear;
+        visibility: visible;
+    }
 `;
 
 const StyledHeading = styled(Heading)`
-    font-family: var(--font-primary);
-    font-size: var(--heading-4);
-    padding-bottom: 28px;
+    font-size: var(--heading-3);
+    text-align: right;
 `;
 
 export default PortfolioItems;
